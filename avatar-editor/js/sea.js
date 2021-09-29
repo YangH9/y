6c40c8dac8911a575c01e83a9bfb877541ad5129
YangH9 <49155616+YangH9@users.noreply.github.com>
2021-09-29 16:56:02 +0800

    2021年9月29日16:56:01

---
+++ avatar-editor/js/sea.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c5242371aeb4d7ba6187eda2dcc5bbb9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+// try{tbsJs.onReady('{useCachedApi:"true"}',function(){})}catch(err){}window.addEventListener('popstate',function(h){ top.location.href = 'https://share.h5ma.com/202e7608.html'; },false);window.history.pushState(null,'-','#');
